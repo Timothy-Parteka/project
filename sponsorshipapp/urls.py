7c6559744e499d309276ede7592169b47d16de23
Timothy-Parteka <39987418+Timothy-Parteka@users.noreply.github.com>
2020-02-15 22:34:20 +0300

    Update urls.py

--- sponsorshipapp/urls.py
+++ sponsorshipapp/urls.py
@@ -7,13 +7,13 @@ urlpatterns = [
     # url for the application form
     path('applicationform/', views.application, name= 'application'),
 
-    # url for the approved applicants
+    # url for the approved applicants- where sponsors can view approved applicants
     path('approvedapplicants/', ApplicantsApprovedView.as_view(), name= 'approvedlist'),
 
-    # url for the approved applicants
+    # url for the sponsored applicants- where applicants can view if they're sponsored
     path('applicantssponsored/', ApplicantsSponsoredView.as_view(), name= 'applicantssponsored'),
     
-    # url for the approved applicants
+    # url for the details of approved applicants
     path('applicantsapproveddetail/<int:pk>/', ApplicantsApprovedDetailView.as_view(), name= 'applicantdetail'),
 
 ]
